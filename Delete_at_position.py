a2e61a42dd2b49df1689b465d0df2955b67e3a64
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-11-21 09:24:49 -0600

    Create Delete_at_position.py

---
+++ Delete_at_position.py
@@ -0,0 +1,30 @@
+"""
+ Delete Node at a given position in a linked list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method. 
+"""
+
+def Delete(head, position):
+    if(head.next = None):
+        return None
+    else:
+        node = head
+        ctr = position
+        if(ctr>0):
+            prev_node = node
+            node = node.next
+            ctr = ctr - 1
+        prev_node.next = node.next
+        
+  
+  
+  
+  
+  
