2ad598b1660103239f5c5411f93321ec4356f237
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-11-29 15:44:27 -0600

    Update 10_Return_Value_of_a_node_at_given_position_from_tailNode.py

--- 10_Return_Value_of_a_node_at_given_position_from_tailNode.py
+++ 10_Return_Value_of_a_node_at_given_position_from_tailNode.py
@@ -1,1 +1,47 @@
+#Body
+"""
+ Get Node data of the Nth Node from the end.
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the node data of the linked list in the below method.
+"""
+
+def GetNode(head, position):
+    
+    node = head
+    
+    #Calculate Number of Nodes in the link List
+    
+    no_of_nodes = 1
+    if(node == None):
+        no_of_nodes = 0
+    else:
+        while(node.next != None):
+            no_of_nodes = no_of_nodes + 1
+            node = node.next
+        
+    #Assign node to head node    
+    node = head
+    if(no_of_nodes == 0):
+        return(None)
+    else:        
+        for i in range(1, no_of_nodes - position, 1):
+                node = node.next
+        return(node.data)
+    
+  
+  
+  
+  
+  
+  
+  
+  
 
